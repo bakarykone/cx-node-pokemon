d8d480164418cb6dddf3fac793ec6db40e21b4c5
makan kone <bakarykone729@gmail.com>
2020-11-27 10:31:04 +0100

    update server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express");
-
 const server = express();
+const arg = process.argv
 
 
 
 server.listen(4242,function() {
-console.log('Server is listening on http://localhost:4242');
+console.log('Server is listening on http://localhost:' + arg[2] );
 });
